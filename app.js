96c9eb6249b2302650626ec684104a94c7adbd7b
Daniel Oh <danieloh612@gmail.com>
2015-07-27 20:31:10 -0400

    Babel install
    Install babel

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var fs = require('fs');
 
 var app = express();
 
@@ -21,9 +22,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'build')));
+
 
 app.use('/', routes);
 app.use('/users', users);
+
+app.locals.assetBundles = JSON.parse(fs.readFileSync('./webpack-assets.json', 'utf8'));
+console.log(JSON.stringify(app.locals.assetBundles));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -372,7 +372,7 @@
 /* 5 */
 /***/ function(module, exports) {
 
-	module.exports = "It works from content.js.";
+	module.exports = "It works from content.js!";
 
 /***/ }
 /******/ ]);
